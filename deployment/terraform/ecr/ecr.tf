2792d5a4bef838f8bb8449bd3ff0144a2aac2ec0
Rohit Zinzuvadia <rohit.zinzuvadia@gmail.com>
2022-08-21 14:09:09 +0530

    Change ecs resource name in terraform script

--- deployment/terraform/ecr/ecr.tf
+++ deployment/terraform/ecr/ecr.tf
@@ -6,10 +6,10 @@ provider "aws" {
   region = "${var.regionName}"
 }
 
-resource "aws_ecr_repository" "spring-boot-sample-app-ecr" {
+resource "aws_ecr_repository" "sample-service-app-ecr" {
   name = "${var.serviceName}"
 }
 output "ecr_repository_name" {
     description = "ECR Repository url"
-    value = aws_ecr_repository.spring-boot-sample-app-ecr.repository_url
+    value = aws_ecr_repository.sample-service-app-ecr.repository_url
 }
